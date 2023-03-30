bb6c5260ec2cc63e36a6544627f8cbb5a6da5d2f
Maxwell Walters <johndoe@example.com>
2023-03-30 13:01:29 -0500

    initial commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+
+
+
+<title>We Don't Byte</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+  <article>
+    <h1><a href="/post">My First Post</a></h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quae, quod, voluptate</p>
+</article>
+
+<article>
+    <h1>my first post</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quae, quod, voluptate</p>
+</article>
+  <article>
+    <h1>my first post</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quae, quod, voluptate</p>
+</article>
+
+<article>
+    <h1>my first post</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quae, quod, voluptate</p>
+</article>
+  <article>
+    <h1>my first post</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quae, quod, voluptate</p>
+</article>
+
+<article>
+    <h1>my first post</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quae, quod, voluptate</p>
+</article>
+</body>
