dc1889f69f97fe5d88bfa809008e69ada0a47e0b
Maxwell Walters <johndoe@example.com>
2023-03-30 20:12:19 -0500

    relationships

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,11 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Post extends Model
 {
   
     use HasFactory;
 
-    // protected $fillable = ['title', 'excerpt', 'body'];
+    protected $fillable = ['title', 'excerpt', 'body', 'category_id', 'slug'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
     
 }
