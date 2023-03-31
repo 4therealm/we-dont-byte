f34d6da430c87e4927e89ff3439fd58761081add
Maxwell Walters <johndoe@example.com>
2023-03-31 08:49:12 -0500

    created seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace Database\Seeders;
-
+use App\Models\Category;
+use App\Models\User;
+use App\Models\Post;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,11 +14,44 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+      User::truncate();
+      Category::truncate();
+      Post::truncate();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+
+     $user = User::factory()->create();
+
+     $personal = Category::create(['name' => 'Personal', 'slug' => 'personal']);
+     $family = Category::create(['name' => 'Family', 'slug' => 'family']);
+     $work = Category::create(['name' => 'Work', 'slug' => 'work']);
+
+
+     Post::create([
+       'title' => 'My First Post',
+       'excerpt' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>',
+       'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>',
+       'category_id' => $personal->id,
+       'user_id' => $user->id,
+       'slug' => 'my-first-post'
+     ]);
+
+      Post::create([
+        'title' => 'My Second Post',
+        'excerpt' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>',
+        'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>',
+        'category_id' => $work->id,
+        'user_id' => $user->id,
+        'slug' => 'my-second-post'  
+      ]);
+
+      Post::create([
+        'title' => 'My Third Post',
+        'excerpt' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>',
+        'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>',
+        'category_id' => $family->id,
+        'user_id' => $user->id,
+        'slug' => 'my-third-post'  
+      ]);
+
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -8,7 +8,7 @@
   {{ $post->title }}
 </h1>
 <p>
-  <a href="/categories/{{ $post->category->slug}}">{{$post->category->name}}</a>
+ By <a href="#">Maxwell Walters</a> in <a href="/categories/{{ $post->category->slug}}">{{$post->category->name}}</a>
 </p>
     <div>
  {!! $post->body !!}
