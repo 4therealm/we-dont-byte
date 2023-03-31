f59c99fdd5049522371361c137ade949afa65d61
Maxwell Walters <johndoe@example.com>
2023-03-31 13:51:25 -0500

    skipping design for now

--- resources/views/components/post-featured-card.blade.php
+++ resources/views/components/post-featured-card.blade.php
@@ -9,9 +9,9 @@ class="transition-colors duration-300 hover:bg-gray-100 border border-black bord
     <div class="flex-1 flex flex-col justify-between">
         <header class="mt-8 lg:mt-0">
             <div class="space-x-2">
-                <a href="#"
+                <a href="/categories/{{ $post->category->slug }}"
                    class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-                   style="font-size: 10px">Techniques</a>
+                   style="font-size: 10px">{{$post->category->name}}</a>
 
                 <a href="#"
                    class="px-3 py-1 border border-red-300 rounded-full text-red-300 text-xs uppercase font-semibold"
@@ -20,11 +20,13 @@ class="transition-colors duration-300 hover:bg-gray-100 border border-black bord
 
             <div class="mt-4">
                 <h1 class="text-3xl">
-                   {{ $post->title }}}
+                  <a href="/posts/{{$post->slug}}">
+                   {{ $post->title }}
+                  </a>
                 </h1>
 
                 <span class="mt-2 block text-gray-400 text-xs">
-                        Published <time>1 day ago</time>
+                        Published <time>{{$post->created_at->diffForHumans()}}</time>
                     </span>
             </div>
         </header>
@@ -37,7 +39,7 @@ class="transition-colors duration-300 hover:bg-gray-100 border border-black bord
             </p>
 
             <p class="mt-4">
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              {{ $post->excerpt }}
             </p>
         </div>
 
@@ -45,13 +47,14 @@ class="transition-colors duration-300 hover:bg-gray-100 border border-black bord
             <div class="flex items-center text-sm">
                 <img src="/images/lary-avatar.svg" alt="Lary avatar">
                 <div class="ml-3">
-                    <h5 class="font-bold">Lary Laracore</h5>
-                    <h6>Mascot at Laracasts</h6>
+                  <a href="/authors/{{$post->author->username}}">
+                    <h5 class="font-bold">{{$post->author->name}}</h5>
+                  </a>
                 </div>
             </div>
 
             <div class="hidden lg:block">
-                <a href="#"
+                <a href="/posts/{{ $post->slug }}"
                    class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8"
                 >Read More</a>
             </div>
