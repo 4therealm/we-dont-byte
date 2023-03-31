4ea1441356683c7987554aa9fe7f3b97694bfd43
Maxwell Walters <johndoe@example.com>
2023-03-30 20:34:08 -0500

    adjusting lazy loading for posts->categories

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 use Illuminate\Support\Facades\File;
@@ -21,15 +22,16 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/posts', function () {
   return view('posts', [
-      'posts' => Post::all()
+      'posts' => Post::with('category')->latest()->get()
   ]);
 });
-
-
-
-
 Route::get('posts/{post:slug}', function (Post $post) {// Post::where('slug', $post)->firstOrFail
   return view('post', [
     'post' => $post
+  ]);
+});
+Route::get('/categories/{category:slug}', function (Category $category) {
+  return view('posts', [
+      'posts' => $category->posts
   ]);
 });
